04c7e24000b1684889e94f9e56a4f64c7a829b69
Nora01K <nora.klemp@t-online.de>
2023-12-08 22:02:06 +0100

    start imprint

---
+++ src/routes/imprint/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+</script>
+
+<main class="flex flex-col justify-center items-center mt-3">
+	<div class="card bg-red p-2 w-[80vw] mb-5">
+		<h1 class="font-bold text-2xl">Impressum Frontend</h1>
+		<div class="mt-2">
+			<p class="font-bold mb-1">Adresse:</p>
+			<div class="ml-2">
+				<p>DHBW Mannheim</p>
+				<p>Coblitzallee 1-9</p>
+				<p>68163 Mannheim</p>
+				<p>Deutschland</p>
+			</div>
+		</div>
+		<div class="mt-2 mb-3">
+			<p class="font-bold mb-1">Kontakt:</p>
+			<div class="ml-2">
+				<p>Telefon: +49 621 4105-1171</p>
+				<p>Fax: +49 621 4105-1249</p>
+				<p>E-Mail: info@dhbw-mannheim.de</p>
+			</div>
+		</div>
+        <div>
+            <p class="mt-3">Hinweis:</p>
+            <p class="text-justify mr-2">Diese Webseite wurde im Rahmen eines Studentenprojekts entwickelt. Das Projektteam besteht aus Studierenden, die im Rahmen ihres Studiums an der DHBW an der Umsetzung dieser Webseite beteiligt waren. Die Studierenden übernehmen keine Haftung für die Richtigkeit, Vollständigkeit und Aktualität der bereitgestellten Informationen.</p>
+            <p class="mt-3">Haftung für Inhalte:</p>
+            <p class="text-justify mr-2">Als Projektteam übernehmen wir keine Gewähr für die Aktualität, Vollständigkeit oder Richtigkeit der bereitgestellten Informationen auf dieser Webseite. Gemäß § 7 Abs.1 TMG sind wir als Dienstanbieter für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+            <p class="mt-3">Haftung für Links:</p>
+            <p class="text-justify mr-2">Unsere Webseite enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Daher können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+            <p class="mt-3">Urheberrecht</p>
+            <p class="text-justify mr-2">Die durch das Projektteam erstellten Inhalte und Werke auf dieser Seite unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Projektteam erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Solltest du trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+        </div>
+	</div>
+	<div class="card bg-red p-2 w-[80vw]">
+		<h1>Impressum Backend</h1>
+		<p>Impressum Text</p>
+	</div>
+</main>
