4f77919704d05c1a443482a38105b7ef22fa0e8c
Marcel Bulling <marcel_bulling@gmx.de>
2023-12-11 19:18:32 +0100

    fix eslint error

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,8 +1,5 @@
 import type { PageServerLoad } from './$types';
 
-
 export const load = (async () => {
-
-
-	return {  };
+	return {};
 }) satisfies PageServerLoad;
--- tests/register.test.ts
+++ tests/register.test.ts
@@ -1,13 +1,13 @@
 import { expect, test } from '@playwright/test';
 
 // email tests
-test('email must contain \'@\'', async ({ page }) => {
+test("email must contain '@'", async ({ page }) => {
 	await page.goto('/register');
 	await page.getByPlaceholder('Email').fill('notatanemail.de');
 	await expect(page.getByText('*This is no vaild email')).toBeVisible();
 });
 
-test('email must contain \'.\'', async ({ page }) => {
+test("email must contain '.'", async ({ page }) => {
 	await page.goto('/register');
 	await page.getByPlaceholder('Email').fill('notat@anemaildotde');
 	await expect(page.getByText('*This is no vaild email')).toBeVisible();
@@ -27,19 +27,19 @@ test('email must not contain special characters', async ({ page }) => {
 	await expect(page.getByText('*This is no vaild email')).toBeVisible();
 });
 
-test('email must contain something before \'@\'', async ({ page }) => {
+test("email must contain something before '@'", async ({ page }) => {
 	await page.goto('/register');
 	await page.getByPlaceholder('Email').fill('@anemail.de');
 	await expect(page.getByText('*This is no vaild email')).toBeVisible();
 });
 
-test('email must contain something after \'@\'', async ({ page }) => {
+test("email must contain something after '@'", async ({ page }) => {
 	await page.goto('/register');
 	await page.getByPlaceholder('Email').fill('not@.de');
 	await expect(page.getByText('*This is no vaild email')).toBeVisible();
 });
 
-test('email must contain two chars after \'.\'', async ({ page }) => {
+test("email must contain two chars after '.'", async ({ page }) => {
 	await page.goto('/register');
 	await page.getByPlaceholder('Email').fill('not@anemail.d');
 	await expect(page.getByText('*This is no vaild email')).toBeVisible();
@@ -55,7 +55,7 @@ test('email accepted when valid', async ({ page }) => {
 test('username must not be empty', async ({ page }) => {
 	await page.goto('/register');
 	await page.getByPlaceholder('Username').fill('');
-	await expect(page.getByRole("button", {name: "Register"})).toBeDisabled();
+	await expect(page.getByRole('button', { name: 'Register' })).toBeDisabled();
 });
 
 test('username must not contain spaces', async ({ page }) => {
@@ -133,7 +133,7 @@ test('passwords must match', async ({ page }) => {
 	await page.goto('/register');
 	await page.getByPlaceholder('Password').first().fill('Password1!');
 	await page.getByPlaceholder('Password').last().fill('Password2!');
-	await expect(page.getByRole("button", {name: "Register"})).toBeDisabled();
+	await expect(page.getByRole('button', { name: 'Register' })).toBeDisabled();
 });
 
 // full test
@@ -143,5 +143,5 @@ test('register button is enabled when all inputs are valid', async ({ page }) =>
 	await page.getByPlaceholder('Username').fill('username');
 	await page.getByPlaceholder('Password').first().fill('Password1!');
 	await page.getByPlaceholder('Password').last().fill('Password1!');
-	await expect(page.getByRole("button", {name: "Register"})).toBeEnabled();
+	await expect(page.getByRole('button', { name: 'Register' })).toBeEnabled();
 });
