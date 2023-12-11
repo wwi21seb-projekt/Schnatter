cf7e0dd20ec0c8bb0568ad6aaa8b706a8477da3d
Marcel Bulling <marcel_bulling@gmx.de>
2023-12-11 22:41:36 +0100

    update format

--- src/routes/verify/+page.svelte
+++ src/routes/verify/+page.svelte
@@ -3,35 +3,38 @@
 
 	function handleInput(event: Event) {
 		const newValue = (event.target as HTMLInputElement).value.replace(/\D/g, '');
-        verifyInput = newValue.slice(0, 6);
+		verifyInput = newValue.slice(0, 6);
 	}
 
-    function handleSubmit(){
-        console.log("test")
-
-    }
-    $: formCorrectCode = verifyInput.length == 6
+	function handleSubmit() {
+		console.log('test');
+	}
+	$: formCorrectCode = verifyInput.length == 6;
 </script>
 
 <main class="flex justify-center items-center w-screen h-screen">
 	<div class="card w-[40vw] h-[50vh] p-5">
 		<h2 class="h2">Verify your Account</h2>
 
-		<div class="w-full h-full flex flex-col items-center justify-around">
-            <p>Please verify your account. You have received an email to the specified email address! Please enter the code it contains here:</p>
+		<form class="w-full h-full flex flex-col items-center justify-around">
+			<p>
+				Please verify your account. You have received an email to the specified email address!
+				Please enter the code it contains here:
+			</p>
 			<input
 				bind:value={verifyInput}
 				on:input={handleInput}
 				class="input variant-form-material w-[80px]"
-				
 				placeholder="XXXXXX"
 			/>
-            <div class="flex flex-row items-center justify-around w-2/5 ">
-                
-                <a href="/"><button class="btn variant-filled-surface">Ask later</button></a>
-                <button on:click={handleSubmit} disabled={!formCorrectCode} class="btn variant-filled-primary" >Verify</button>
-            </div>
-		</div>
-
+			<div class="flex flex-row items-center justify-around w-2/5">
+				<a href="/"><button class="btn variant-filled-surface">Ask later</button></a>
+				<button
+					on:click={handleSubmit}
+					disabled={!formCorrectCode}
+					class="btn variant-filled-primary">Verify</button
+				>
+			</div>
+		</form>
 	</div>
 </main>
