2fdd759556da1500fa28c5a32ece1f9931e58e54
Marcel Bulling <marcel_bulling@gmx.de>
2023-12-12 20:36:05 +0100

    update toats messages

--- src/routes/verify/Toasts.ts
+++ src/routes/verify/Toasts.ts
@@ -9,25 +9,25 @@ export const InternalServerError: ToastSettings = {
 export const verifySuccess: ToastSettings = {
 	message: 'Account successfully verified',
 	timeout: 20000,
-	background: 'variant-filled-variant-filled-success'
+	background: 'variant-filled-success'
 };
 export const resendTokenSuccess: ToastSettings = {
 	message: 'We have sent you a new code to your email address',
 	timeout: 20000,
-	background: 'variant-filled-variant-filled-success'
+	background: 'variant-filled-success'
 };
 export const tokenNotFound: ToastSettings = {
 	message: 'The code is not correct',
 	timeout: 20000,
-	background: 'variant-filled-variant-filled-success'
+	background: 'variant-filled-error'
 };
 export const tokenExpierd: ToastSettings = {
 	message: 'The code has expired. We have sent you a new one!',
 	timeout: 20000,
-	background: 'variant-filled-variant-filled-success'
+	background: 'variant-filled-error'
 };
 export const otherError: ToastSettings = {
 	message: 'Wie havea problem. Please try again later!',
 	timeout: 20000,
-	background: 'variant-filled-variant-filled-success'
+	background: 'variant-filled-error'
 };
