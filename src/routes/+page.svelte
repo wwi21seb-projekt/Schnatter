1412980368c9e85ffb2490081d30833dc1a649b5
Marcel Bulling <marcel_bulling@gmx.de>
2023-12-14 16:07:20 +0100

    update format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { token } from '$lib/Store';
 	import Login from '../components/Login.svelte';
 	let loginToken = '';
-	token.subscribe((prev_val) => (loginToken = prev_val))
+	token.subscribe((prev_val) => (loginToken = prev_val));
 </script>
 
 <main class="">
@@ -11,9 +11,9 @@
 		<div class="card col-span-4"></div>
 		<div class="grid grid-rows-6 grid-flow-col gap-4">
 			{#if loginToken == ''}
-			<div class="card row-start-1"><Login /></div>
+				<div class="card row-start-1"><Login /></div>
 			{/if}
-			
+
 			<div class="card row-span-5"></div>
 		</div>
 	</div>
