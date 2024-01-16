e0b7c9b330b34db8eca12db0c1759ee31dcb27d5
Marcel Bulling <marcel_bulling@gmx.de>
2024-01-16 19:37:17 +0100

    custom errors verify and register

--- src/routes/imprint/+page.server.ts
+++ src/routes/imprint/+page.server.ts
@@ -1,19 +1,23 @@
 import { serverURL } from '$lib/Store';
+import type { Imprint } from '$lib/types/Imprint';
 import type { PageServerLoad } from './$types';
 
-type imprint = {
-	text: string;
-};
+
 
 export const load = (async () => {
+	const imprint: Imprint = {
+		text: '',
+		status: 0
+	};
 	let serverUrl: string = '';
 	serverURL.subscribe((prev_val) => (serverUrl = prev_val));
 	const url: string = serverUrl + '/imprint';
 	const response = await fetch(url);
-	const responseObject: imprint = await response.json();
-	const imprintText: string = responseObject.text;
+	const responseObject = await response.json();
+	imprint.text = responseObject.text;
+	imprint.status = responseObject.status;
 
 	return {
-		text: imprintText
+		Imprint: imprint
 	};
 }) satisfies PageServerLoad;
