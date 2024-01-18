cd950786ddfcfdecba5a87932d3470b04fd3f70f
mabu2807 <marcel_bulling@gmx.de>
2024-01-18 14:06:18 +0100

    code-review

--- src/lib/Store.ts
+++ src/lib/Store.ts
@@ -4,7 +4,7 @@ import { writable } from 'svelte/store';
 export const registerUsername = writable('');
 
 //https://server-beta.de/api
-//https://alpha.c930.net/api
+//https://server-alpha.tech/api
 //http://localhost:3000
 // local network http://192.168.0.126:3000/api
 export const serverURL = writable('http://localhost:3000/api');
