e0bed679d26d27c42a7f86b9d23eb009aa0867a3
Marcel Bulling <marcel_bulling@gmx.de>
2024-01-31 12:15:35 +0100

    format

--- src/routes/profile/requests.ts
+++ src/routes/profile/requests.ts
@@ -92,8 +92,8 @@ export async function loadPosts(token: string, postData: UserPostFetchResponse, 
 	const posts: UserPostFetchResponse = await response.json();
 	postData.pagination = posts.pagination;
 	console.log(posts);
-	if (posts.records !== null){
-	postData.records = postData.records.concat(posts.records);
+	if (posts.records !== null) {
+		postData.records = postData.records.concat(posts.records);
 	}
 	posts.statusCode = await response.status;
 
