e419df7223e6f6fe0711a6d092527ee3bfb32b4d
Schokobonlean <s212529@student.dhbw-mannheim.de>
2024-05-21 12:30:18 +0200

    sollte funktionieren

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type { PostStructure, TextColorPost, LikeObjectStructure } from '$lib/types/Post';
 	import Icon from '@iconify/svelte';
-	import { Avatar } from '@skeletonlabs/skeleton';
+	import { Avatar, getModalStore, type ModalSettings } from '@skeletonlabs/skeleton';
 	import { token } from '$lib/Store';
 	import { get } from 'svelte/store';
 	import { t } from '../i18n';
@@ -10,6 +10,8 @@
 	import { getLocationCity } from '$lib/utils/GeoLocationUtils';
 
 	export let postData;
+
+	const modalStore = getModalStore();
 
 	let post: PostStructure = postData;
 	let postDate: string = '';
@@ -22,6 +24,13 @@
 	let likeObject: LikeObjectStructure = {
 		likeCount: 123,
 		liked: false
+	};
+
+	const modal: ModalSettings = {
+		type: 'component',
+		component: 'modalCreatePost',
+
+		meta: {repostId: post.postId}
 	};
 
 	let newPost: TextColorPost[] = [
@@ -68,9 +77,7 @@
 	}
 
 	function repostHelper() {
-		if (loginToken != '' && loginToken != undefined) {
-			// repost;
-		}
+		modalStore.trigger(modal);
 	}
 
 	function helperHashtagCheck(post: PostStructure) {
@@ -103,7 +110,7 @@
 		</header>
 		<section class="p-4">
 			<p
-				class="h-[15vh] border-solid border-2 border-gray-800 p-1 text-lg overflow-auto"
+				class="h-[15vh] border-solid border-2 border-gray-800 p-1 text-lg h-auto"
 				title="postcontent"
 			>
 				{#each newPost as { hashtagClass, text, wordID } (wordID)}
@@ -113,34 +120,34 @@
 					<header class="card-header w-full flex justify-between items-center">
 						<div class="flex flex-row items-center">
 							<Avatar
-								class="h-[3.5vh] w-[3.5vh] rounded-full mr-1.5"
+								class="h-[5vh] w-[5vh] rounded-full mr-3"
 								src={post.repost.author.profilePictureUrl}
 								initials=""
 							/>
 							<div class="flex flex-col">
 								<a
-									class="text-[0.5rem]"
+									class="text-[0.75rem]"
 									title="repostAuthorUsername"
 									href="/profile?username={post.repost.author.username}"
 									data-sveltekit-preload-data="hover">@{post.repost.author.username}</a
 								>
-								<p class="font-light text-xs text-[0.5rem]" title="repostAuthorNickname">
+								<p class="font-light text-xs text-[0.75rem]" title="repostAuthorNickname">
 									{post.repost.author.nickname}
 								</p>
 							</div>
 						</div>
 						<div class="flex flex-col items-end">
-							<p class="text-xs text-[0.5rem]">{repostLocationString}</p>
-							<p class="text-xs text-[0.5rem]" title="repostPostdate">{repostDate}</p>
+							<p class="text-xs text-[0.75rem]">{repostLocationString}</p>
+							<p class="text-xs text-[0.75rem]" title="repostPostdate">{repostDate}</p>
 						</div>
 					</header>
 					<section class="p-4">
 						<p
-							class="h-[10vh] border-solid border-2 border-gray-800 p-1 text-lg overflow-auto"
+							class="h-[10vh] border-solid border-2 border-gray-800 p-1 text-lg h-auto"
 							title="repostPostcontent"
 						>
 							{#each newRepostPost as { hashtagClass, text, wordID } (wordID)}
-								<span class="{hashtagClass} text-[0.5rem]">{text} </span>
+								<span class="{hashtagClass} text-[0.75rem]">{text} </span>
 							{/each}
 						</p>
 					</section>
