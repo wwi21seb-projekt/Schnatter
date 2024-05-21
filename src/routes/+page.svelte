6b16657f8fc6efa33aacfed0cbe88082f5820a6e
Marcel Bulling <marcel_bulling@gmx.de>
2024-05-21 21:30:04 +0200

    lint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,6 @@
 	import { fetchPosts } from '../lib/FeedPosts';
 	import { t } from '../i18n';
 	import { manageSession } from '$lib/utils/Session';
-	import { subscribeUserToPush } from '../push';
 
 	let slotLimit = 10;
 	let feedType = 'global';
