7b0c7e742a532a67cbba22eb71e78404ce105e47
Marcel Bulling <marcel_bulling@gmx.de>
2024-05-22 15:57:06 +0200

    requests for chat

---
+++ src/lib/types/Chat.ts
@@ -0,0 +1,29 @@
+import type { UUID } from 'crypto';
+import type { Pagination } from './Pagination';
+
+export type ChatStructure = {
+	chatId: UUID;
+	users: ChatUser[];
+};
+
+export type ChatUser = {
+	username: string;
+	nickname: string;
+	profilePictureUrl: string | undefined;
+};
+
+export type ChatMessages = {
+	records: [ChatMessage];
+	pagination: Pagination;
+};
+
+export type ChatMessage = {
+	content: string;
+	username: string;
+	timestamp: string;
+};
+
+export type CreateChat = {
+	chatId: UUID;
+	message: ChatMessage;
+};
